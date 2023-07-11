a1fbc2ccc651b6c14f5a1b5133d04da9c0f0fc54
TheMikal <131578548+TheMikal@users.noreply.github.com>
2023-07-11 02:18:59 -0400

    adding more styling and js code

--- Assets/script.js
+++ Assets/script.js
@@ -7,33 +7,33 @@ when finished, either all the questions or running out of time, the quiz takes t
 // setting the section parents as variables to use later
 const timeBar = document.querySelector(".timeBar");
 const timerEl = document.getElementById("timerEl");
+const scoreEl = document.getElementById('scoreBox');
+const smarties = document.getElementById('smartyList');
 const gameCont = document.querySelector(".gameCont");
+const startBtn = document.querySelector(".startBtn");
 const areYouSmart = document.querySelector(".areYouSmart");
 // for the timer
-const timeRemaining = 60; // Timer duration in seconds
-const gameStart = false;
-
-/*
-gameCont.addEventListener("click",function(event){
-    event.preventDefault();
-    console.log(gameStart);
-    if (gameStart === false) {
-        let gameStart = true;
-        console.log(gameStart);
-    }
-    console.log(gameStart);
-    
-});
-*/
-
+const timeRemaining = 30; // Timer duration in seconds
+let gameStart = false;
+console.log(gameStart);
 
 // Event listener for button click
-gameCont.addEventListener('click', function () {
-    let gameStart = true; // Change the constant value to true
-    let remainingTime = timeRemaining;
-    timerEl.textContent = remainingTime;
+gameCont.addEventListener('click',function (event) {
+    if (event.target.classList.contains('startBtn')){
+        event.stopPropagation();
+        startGame();
+    }
+});
 
-  // Start the timer
+function startGame() {
+    let gameStart = true; // Change the constant value to true
+    console.log(gameStart)
+    // hides the start button when quiz begins
+    startBtn.style.display = "none"
+    let remainingTime = timeRemaining;
+    timerEl.textContent = `Time: ${remainingTime}`;
+
+    // Start the timer
     const timer = setInterval(function () {
     remainingTime--;
     timerEl.textContent = remainingTime;
@@ -42,6 +42,7 @@ gameCont.addEventListener('click', function () {
     switch (remainingTime) {
         case 10:
             timerEl.textContent = 'Hurry up! Only 10 seconds left!';
+            timerEl.style = "color: red"
             break;
         case 5:
             timerEl.textContent = 'Almost there! Just 5 seconds remaining!';
@@ -53,14 +54,23 @@ gameCont.addEventListener('click', function () {
             break;
         default:
         // Display remaining time
-            timerEl.textContent = remainingTime + " seconds remaining.";
+            timerEl.textContent = `${remainingTime} seconds remaining.`;
     }
 
     // Check if the timer has reached 0
     if (remainingTime === 0) {
         clearInterval(timer); // Stop the timer
-        gameStart = false; // Reset the state to false
-        timerEl.textContent = 'Timer completed';
+        let gameStart = false; // Reset the state to false
+        console.log(gameStart);
+        timerEl.textContent = 'Time expired try again.';
+        timerEl.style = "color: black";
+        // changes the text of the start button and shows it again
+        startBtn.textContent = "Try Again!";
+        startBtn.style.display = "flex";
     }
     }, 1000); // Update the timer every second
-});
+};
+
+function bigSadYouAreWrong() {
+/* remainingTime -= # to remove time from timer */
+}
